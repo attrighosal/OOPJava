ee700ec04da6949cdadc45c81aff575ebeda5550
attrighosal <attrighosal@gmail.com>
2022-03-13 21:07:12 +0530

    Added the rest part of code for MusicLibrary

--- src/MusicLibrary/application/Driver.java
+++ src/MusicLibrary/application/Driver.java
@@ -13,7 +13,38 @@ public class Driver {
     int userId, artistId, songId;
 
     registerUsers(sc);
+    registerArtists(sc);
+    releaseSongs(sc);
 
+    do {
+      try {
+        System.out.println("Please choose the operation you'd like to perform: " +
+            "\n1. Play a song." +
+            "\n2. Get top 10 songs of a user." +
+            "\n3. Get top 10 songs overall." +
+            "\n4. Get all songs of an artist.");
+        System.out.print("Your choice : ");
+        int operationChoice = sc.nextInt();
+
+        switch (operationChoice) {
+          case 1:
+            try {
+              System.out.print("Please enter the user ID : ");
+              userId = sc.nextInt();
+              System.out.print("Please enter the song ID : ");
+              songId = sc.nextInt();
+              app.playSong(songId, userId);
+            } catch (Exception ex) {
+              System.out.println(ex.getMessage());
+            }
+            break;
+        }
+      } catch (Exception ex) {
+        System.out.println(ex.getMessage());
+      }
+      System.out.println("Would you like to perform another operation?(Y/n)");
+      addAgain = sc.next();
+    } while (addAgain.equals("Y"));
   }
 
   public static void registerUsers(Scanner sc) {
@@ -44,6 +75,51 @@ public class Driver {
         System.out.println(ex.getMessage());
       }
       System.out.println("Would you like to add another user?(Y/n)");
+      addAgain = sc.next();
+    } while (addAgain.equals("Y"));
+  }
+
+  public static void registerArtists(Scanner sc) {
+    System.out.println("----------- Artist registration -----------");
+    String addAgain;
+    do {
+      try {
+        System.out.println("Please enter user details to add an artist.");
+        System.out.print("First Name : ");
+        String firstName = sc.next();
+        System.out.print("Last Name : ");
+        String lastName = sc.next();
+        app.registerArtists(firstName, lastName);
+      } catch (Exception ex) {
+        System.out.println(ex.getMessage());
+      }
+      System.out.println("Would you like to add another user?(Y/n)");
+      addAgain = sc.next();
+    } while (addAgain.equals("Y"));
+  }
+
+  public static void releaseSongs(Scanner sc) {
+    System.out.println("----------- Releasing Song -----------");
+    String addAgain;
+    do {
+      try {
+        System.out.println("Please enter user details to add an song.");
+        System.out.print("Title  : ");
+        String title = sc.next();
+        System.out.print("Genre : ");
+        String genre = sc.next();
+        System.out.print("Release year : ");
+        int releaseYear = sc.nextInt();
+        System.out.print("Audio language : ");
+        String language = sc.next();
+        System.out.println("Please select an artist(enter the sequence number only)");
+        app.printArtists();
+        int artistId = sc.nextInt();
+        app.releaseSong(artistId, title, genre, releaseYear, language);
+      } catch (Exception ex) {
+        System.out.println(ex.getMessage());
+      }
+      System.out.println("Would you like to add another song?(Y/n)");
       addAgain = sc.next();
     } while (addAgain.equals("Y"));
   }
--- src/MusicLibrary/application/MusicLibrary.java
+++ src/MusicLibrary/application/MusicLibrary.java
@@ -1,5 +1,7 @@
 package MusicLibrary.application;
 
+import MusicLibrary.entities.Artist;
+import MusicLibrary.entities.Song;
 import MusicLibrary.entities.User;
 
 import java.util.Date;
@@ -7,7 +9,20 @@ import java.util.HashMap;
 
 public class MusicLibrary {
 
-  private HashMap<Integer, User> userMap = new HashMap<>();
+  private HashMap<Integer, User> users = new HashMap<>();
+  private HashMap<Integer, Artist> artists = new HashMap<>();
+  private HashMap<Integer, Song> songs = new HashMap<>();
+
+  public void playSong(int songId, int userId) {
+    if(!users.containsKey(userId)){
+      throw new RuntimeException("User is invalid!");
+    }
+    if(!songs.containsKey(songId)){
+      throw new RuntimeException("Invalid song!");
+    }
+    Song song = songs.get(songId);
+    song.markPlayed();
+  }
 
   public User registerUser(String firstName, String lastName,
                            String city, String locality,
@@ -17,7 +32,31 @@ public class MusicLibrary {
         new User.Address(city, locality, state, pinCode);
     User.Contact contact = new User.Contact(emailId, phoneNo);
     User user = new User(firstName, lastName, address, contact, new Date());
-    userMap.put(user.getUserId(), user);
+    users.put(user.getUserId(), user);
     return user;
+  }
+
+  public Artist registerArtists(String firstName, String lastName) {
+    Artist artist = new Artist(firstName, lastName, new Date());
+    artists.put(artist.getArtistId(), artist);
+    return artist;
+  }
+
+  public Song releaseSong(int artistId, String title, String genre,
+                          int releaseYear, String language) throws Exception {
+    if (!artists.containsKey(artistId)) {
+      throw new Exception("Artist doesn't exist");
+    }
+    Song song = new Song(artistId, title, genre, releaseYear, language);
+    songs.put(song.getSongId(), song);
+    Artist artist = artists.get(artistId);
+    artist.releaseSong(song);
+    return song;
+  }
+
+  public void printArtists(){
+    for (Artist artists : artists.values()) {
+      System.out.println(artists.getArtistId() + ", "+ artists.getName());
+    }
   }
 }
